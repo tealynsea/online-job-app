dd7f4d232cb6a87b79787a6b78ab20de308232e3
Teri <tealynsea@hotmail.com>
2014-08-02 12:11:11 -0600

    completed 1 - 4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -8,22 +9,59 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
-app.get('/', function(req, res) {
-	res.render('index');
+//  generated with refactoru-express generator
+ app.get('/', function(req, res) {
+ 	res.render('index');
+ });
+
+mongoose.connect('mongodb://localhost/disney');
+
+//part 2 question 5.  Setting up schema
+var Applicant = mongoose.model('Applicant', {
+	name: String,
+	bio: String,
+	skills: String,
+	year: String,
+	why: String
 });
+
+
 
 // displays a list of applicants
 app.get('/applicants', function(req, res){
-	res.render('applicants')
-	console.log('req.body')
+	
+	// display data from database: PART: IV
+		Applicant.find(function(error, data) {
+			if (error)
+				console.log('cannot read applicants');
+			else
+				res.render('applicants', {applicantData: data} );
+		});
+	
+
 });
 
 // creates and applicant
 app.post('/applicant', function(req, res){
 	// Here is where you need to get the data
 	// from the post body and store it in the database
-	res.send('No funciona');
+	console.log(req.body);
+	res.render('./success');
+
+	//store data from post body.
+	var applicants = new Applicant ({
+		name: req.body.name,
+		bio: req.body.bio,
+		skills: req.body.skills,
+		year: req.body.year,
+		why: req.body.why
+	});
+	applicants.save()
 });
+
+// app.get('/success', function(req, res) {
+// 	res.render('/success')
+// })
 
 var server = app.listen(8441, function() {
 	console.log('Express server listening on port ' + server.address().port);
